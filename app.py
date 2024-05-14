79062b879ebd24c210c3db3e9eb13701d61a5d02
eylulozatman <eylulozatman@gmail.com>
2024-05-14 15:32:08 +0300

    rec system improvemnt

--- app.py
+++ app.py
@@ -3,11 +3,24 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 from recipeRecDTO import RecipeRecommendationObject
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import requests
+import json
 
 app = Flask(__name__)
 
+sas_token = "sp=racwdyti&st=2024-05-14T12:06:34Z&se=2024-06-04T20:06:34Z&sv=2022-11-02&sr=b&sig=%2FYZk57JEkcpWlOKhm9rl5y2roVMQbwl%2Fa%2FgyPS5uL5A%3D"
+blob_url = "https://blobrecipeimages.blob.core.windows.net/data-set-kaggle/recipes_with_no_tags_and_cuisine.csv?" + sas_token
+# Blob Service Client oluştur
+blob_service_client = BlobServiceClient(account_url="https://blobrecipeimages.blob.core.windows.net", credential=sas_token)
+
+# Container Client oluştur
+container_client = blob_service_client.get_container_client("data-set-kaggle")
+
+
+
 # Load recipe data
-recipe_data = pd.read_csv('https://blobrecipeimages.blob.core.windows.net/data-set-kaggle/recipes_with_no_tags_and_cuisine.csv')
+recipe_data = pd.read_csv(blob_url)
 recipe_data = recipe_data.dropna(subset=['ingredients', 'cuisine_path'])
 recipe_data['total_time'] = recipe_data['total_time'].astype(str)
 recipe_data['combined_features'] = recipe_data['total_time'] + ' ' + recipe_data['ingredients'] + ' ' + recipe_data['cuisine_path']
@@ -17,14 +30,25 @@ tfidf_vectorizer = TfidfVectorizer(stop_words='english')
 tfidf_matrix = tfidf_vectorizer.fit_transform(recipe_data['combined_features'])
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-
-def get_recommendations(title):
-   
-    # Check if the query is a substring of any recipe names
-    matching_recipes = recipe_data[recipe_data['recipe_name'].str.contains(title, case=False)]
-
+def get_recommendations(title=None, ingredients=None):
+    if title is None and ingredients is None:
+        return "Lütfen en az bir parametre belirtin."
+    
+    if title is None and ingredients:
+        # Sadece ingredients parametresine göre eşleşen tarifleri bul
+        matching_recipes = recipe_data[recipe_data['combined_features'].str.contains(ingredients, case=False)]
+    elif title and ingredients is None:
+        # Sadece title parametresine göre eşleşen tarifleri bul
+        matching_recipes = recipe_data[recipe_data['recipe_name'].str.contains(title, case=False)]
+    else:
+        # Hem title hem de ingredients parametrelerine göre eşleşen tarifleri bul
+        title_matches = recipe_data[recipe_data['recipe_name'].str.contains(title, case=False)]
+        ingredients_matches = recipe_data[recipe_data['combined_features'].str.contains(ingredients, case=False)]
+        matching_recipes = pd.concat([title_matches, ingredients_matches]).drop_duplicates()
+    
     if matching_recipes.empty:
-        return print(f"No recipes found for '{title}'. Try a different search term.")
+        print(f"No recipes found for '{title}' with provided ingredients. Try a different search term or ingredients.")
+        return []
 
     # Use the first matching recipe as the selected recipe
     selected_recipe_row = matching_recipes.iloc[0]
@@ -32,14 +56,17 @@ def get_recommendations(title):
     # Get the index of the selected recipe
     idx = selected_recipe_row.name
 
-    # Get the pairwise similarity scores based on ingredients only
-    sim_scores = list(enumerate(cosine_sim[idx]))
+    # Get the pairwise similarity scores based on ingredients and cuisine
+    combined_features_with_ingredients = recipe_data['total_time'] + ' ' + recipe_data['ingredients'] + ' ' + recipe_data['cuisine_path'] + ' ' + ingredients
+    tfidf_matrix_with_ingredients = tfidf_vectorizer.fit_transform(combined_features_with_ingredients)
+    cosine_sim_with_ingredients = linear_kernel(tfidf_matrix_with_ingredients, tfidf_matrix_with_ingredients)
+    sim_scores = list(enumerate(cosine_sim_with_ingredients[idx]))
 
     # Sort the recipes based on similarity scores
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
     # Get the top 5 similar recipes
-    sim_recipes = sim_scores[1:4]  # Adjusted to get top 20 similar recipes
+    sim_recipes = sim_scores[1:6]  # Top 5 similar recipes
 
     # Get the recipe indices
     recipe_indices = [i[0] for i in sim_recipes]
@@ -56,6 +83,7 @@ def get_recommendations(title):
     return recommended_recipes
 
 
+
 @app.route('/hello', methods=['GET'])
 def hello():
     return "hellooooo"
@@ -64,11 +92,41 @@ def hello():
 @app.route('/get-recommendations', methods=['GET'])
 def recommend():
     title = request.args.get('title')
-    recommendations = get_recommendations(title)
+    ingredients = request.args.get('ingredients')
+    
+    if ingredients and ":" in ingredients:
+        ingredients = formatDataIng(ingredients)
 
-    # Önerileri JSON formatında geri döndür
+    recommendations = get_recommendations(title, ingredients)
+
+    if not recommendations:
+        print(f"No recipes found for '{title}' with provided ingredients. Try a different search term or ingredients.")
+        return jsonify([])
+
     return jsonify({'recommendations': [vars(recommendation) for recommendation in recommendations]})
 
+
+def formatDataIng(ingredients):
+    # Boş bir dize oluştur
+    formatted_ingredients = ""
+    
+    # Her bir malzeme için döngü başlat
+    for ingredient in ingredients.split(','):
+        # Malzemeyi ve ölçeği ayır
+        parts = ingredient.strip().split(':')
+        # Malzemeyi al
+        if len(parts) > 1:
+            ingredient_name = parts[0].strip()  # Sadece malzeme adını al
+            formatted_ingredients += ingredient_name + ", "
+        else:
+            formatted_ingredients += parts[0].strip() + ", "
+
+    # Son virgülü ve boşluğu kaldır
+    formatted_ingredients = formatted_ingredients.rstrip(", ")
+
+    return formatted_ingredients
+
+    
 
 
 def create_recommendation_object(recipe_row):
@@ -77,13 +135,16 @@ def create_recommendation_object(recipe_row):
         ingredients=recipe_row['ingredients'],
         description=recipe_row['directions'],  
         cuisine=recipe_row['cuisine_path'],  
-        timing=format_time(recipe_row['total_time']),  
+        timing=recipe_row['total_time'],
         photoPathURL=recipe_row['img_src']
     )
 
 
 @app.route('/add-recipe-to-dataset', methods=['POST'])
 def add_recipe_to_dataset():
+
+    global recipe_data  # Declare recipe_data as global
+
     data = request.json
 
     # Extract data from request body
@@ -105,33 +166,34 @@ def add_recipe_to_dataset():
     # Process the data and add it to the dataset
     # You can perform any necessary processing here, such as data validation and formatting
     # Then add the recipe to the dataset
-    new_recipe = {
-        'recipe_name': recipe_name,
-        'prep_time': prep_time,
-        'cook_time': cook_time,
-        'total_time': total_time,
-        'servings': servings,
-        'yield': yield_value,
-        'ingredients': ingredients,
-        'directions': directions,
-        'rating': rating,
-        'url': url,
-        'cuisine_path': cuisine_path,
-        'nutrition': nutrition,
-        'timing': timing,
-        'img_src': img_src
-    }
+    new_recipe = pd.DataFrame.from_dict({
+        'recipe_name': [recipe_name],
+        'prep_time': [prep_time],
+        'cook_time': [cook_time],
+        'total_time': [total_time],
+        'servings': [servings],
+        'yield': [yield_value],
+        'ingredients': [ingredients],
+        'directions': [directions],
+        'rating': [rating],
+        'url': [url],
+        'cuisine_path': [cuisine_path],
+        'nutrition': [nutrition],
+        'timing': [timing],
+        'img_src': [img_src]
+    })
 
+    recipe_data = pd.concat([recipe_data, new_recipe], ignore_index=True)
 
-    recipe_data.append(new_recipe, ignore_index=True)
+    # Dosyayı bloba yükle
+    data = recipe_data.to_csv(index=False)
+    blob_client = container_client.get_blob_client(blob="recipes_with_no_tags_and_cuisine.csv")
+    blob_client.upload_blob(data, overwrite=True)
 
-    # Dataseti tekrar kaydet
-    recipe_data.to_csv('recipes_with_no_tags_and_cuisine_updated.csv', index=False)
-
+    print("Veri başarıyla bloba yüklendi.")
 
     return jsonify({"message": "Recipe added to dataset successfully"})
-
-
+ 
 def format_time(minutes):
     if not minutes:
         return ''
